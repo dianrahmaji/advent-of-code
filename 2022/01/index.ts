ba2f4294853d035bc6b1acc63a9fcad1f5dad2b0
Dian Rahmaji <dianrahmaji@gmail.com>
2023-01-25 20:49:39 +0700

    solve 2022/01

--- 2022/01/index.ts
+++ 2022/01/index.ts
@@ -1,3 +1,21 @@
 import { getInput } from "../../utils";
 
-console.log(getInput(__dirname));
+const content = getInput(__dirname)
+  .split("\n\n")
+  .map((elves) =>
+    elves
+      .split("\n")
+      .map((cal) => parseInt(cal))
+      .reduce((prev, acc) => prev + acc, 0)
+  )
+  .sort((a, b) => b - a);
+
+let result;
+
+/**Part 1 */
+result = content[0];
+console.log("Part 1", result);
+
+/**Part 2 */
+result = content.slice(0, 3).reduce((prev, acc) => prev + acc, 0);
+console.log("Part 2", result);
