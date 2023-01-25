d905bf15c96552fb8f5d5024987b1a3026806f9a
Dian Rahmaji <dianrahmaji@gmail.com>
2023-01-25 23:26:02 +0700

    solve 2022/03 part 2

--- 2022/03/index.ts
+++ 2022/03/index.ts
@@ -2,24 +2,44 @@ import { getInput } from "../../utils";
 
 const content = getInput(__dirname);
 
-function getSharedItem(first: string, second: string): string {
+function getSharedItem(first: string, second: string): string | undefined {
   let shared = "";
   for (let i = 0; i < first.length; i++) {
     for (let j = 0; j < second.length; j++) {
-      if (first[i] === second[j]) shared = first[i];
+      if (first[i] === second[j]) return (shared = first[i]);
     }
   }
-
-  return shared;
 }
 
-function getPriority(item: string): number {
+function getSharedItem2(
+  first: string,
+  second: string,
+  third: string
+): string | undefined {
+  let shared = "";
+
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < second.length; j++) {
+      for (let k = 0; k < third.length; k++)
+        if (
+          first[i] === second[j] &&
+          first[i] === third[k] &&
+          second[j] === third[k]
+        )
+          return (shared = first[i]);
+    }
+  }
+}
+
+function getPriority(item?: string): number {
+  if (item === undefined) return 0;
   let ascii = item.charCodeAt(0);
 
   if (ascii <= 90) return ascii - 38;
   return ascii - 96;
 }
 
+/**Part 1 */
 const result = content
   .split("\n")
   .map((rucksack) =>
@@ -32,3 +52,20 @@ const result = content
   .reduce((prev, acc) => prev + acc, 0);
 
 console.log(result);
+
+/**Part 2 */
+const teams: string[][] = [];
+
+content.split("\n").forEach((rucksack, idx) => {
+  if (idx % 3 === 0) {
+    teams.push([rucksack]);
+    return;
+  }
+  teams[teams.length - 1].push(rucksack);
+});
+
+const result2 = teams
+  .map((rucksacks) => getSharedItem2(rucksacks[0], rucksacks[1], rucksacks[2]))
+  .map((item) => getPriority(item))
+  .reduce((prev, acc) => prev + acc, 0);
+console.log(result2);
