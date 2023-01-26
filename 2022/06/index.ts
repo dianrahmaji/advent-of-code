fd857149179f6a4f69fdacbb4b00953f90e16dc9
Dian Rahmaji <dianrahmaji@gmail.com>
2023-01-26 19:41:52 +0700

    solve 2022/06 part 2

--- 2022/06/index.ts
+++ 2022/06/index.ts
@@ -2,16 +2,16 @@ import { getInput } from "../../utils";
 
 const content = getInput(__dirname);
 
-function getMarker(datastream: string) {
+function getMarker(datastream: string, position: number) {
   let sequences: string[] = [];
 
   let i = 0;
   for (; i < datastream.length; i++) {
     sequences.push(datastream[i]);
 
-    if ([...new Set(sequences)].length === 4) break;
+    if ([...new Set(sequences)].length === position) break;
 
-    if (sequences.length === 4) sequences.shift();
+    if (sequences.length === position) sequences.shift();
   }
 
   return i + 1;
@@ -20,8 +20,12 @@ function getMarker(datastream: string) {
 /** Demo */
 // const result = content.split("\n").map((v) => getMarker(v));
 
-/**Part 1 */
-const result = getMarker(content);
+/** Part 1 */
+const result = getMarker(content, 4);
 console.log(result);
+
+/** Part 2 */
+const result2 = content.split("\n").map((v) => getMarker(v, 14));
+console.log(result2);
 
 //TODO: Create utility function to find duplication in array
