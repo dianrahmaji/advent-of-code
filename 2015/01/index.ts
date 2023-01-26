341e5a9cedd9e3562d07e5b34c71477364e4fc83
Dian Rahmaji <dianrahmaji@gmail.com>
2023-01-26 09:07:41 +0700

    solve 2015/01 part 2

--- 2015/01/index.ts
+++ 2015/01/index.ts
@@ -9,3 +9,22 @@ const result1 = content.split("").reduce((prev, curr) => {
 }, 0);
 
 console.log(result1);
+
+/**Part 2 */
+let result2 = "";
+let position = 0;
+const movements = content.split("");
+
+for (let i = 0; i < movements.length; i++) {
+  if (position === -1) break;
+
+  const movement = movements[i];
+  const instruction = movement === "(" ? 1 : -1;
+
+  position += instruction;
+  result2 += movement;
+}
+
+const instructionNumber = result2.length;
+
+console.log(instructionNumber);
