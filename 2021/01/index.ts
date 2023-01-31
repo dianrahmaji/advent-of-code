c6fb93d034a8d3e735131dbbfb55cf7a75ead0d6
Dian Rahmaji <dianrahmaji@gmail.com>
2023-01-31 19:16:23 +0700

    solve 2021/01 part 2

--- 2021/01/index.ts
+++ 2021/01/index.ts
@@ -4,6 +4,20 @@ const content = getInput(__dirname)
   .split("\n")
   .map((v) => parseInt(v));
 
+function createMeasurementWindow(measurement: number[]): number[] {
+  const measurementWindow: number[] = [];
+
+  for (let i = 0; i < measurement.length - 2; i++) {
+    let sum = 0;
+    for (let j = i; j < i + 3; j++) {
+      sum += measurement[j];
+    }
+    measurementWindow.push(sum);
+  }
+
+  return measurementWindow;
+}
+
 /**Part 1 */
 let result = 0;
 for (let i = 1; i < content.length; i++) {
@@ -11,3 +25,11 @@ for (let i = 1; i < content.length; i++) {
 }
 
 console.log(result);
+
+/**Part 2 */
+let result2 = 0;
+const window = createMeasurementWindow(content);
+for (let i = 1; i < window.length; i++) {
+  if (window[i] > window[i - 1]) result2++;
+}
+console.log(result2);
