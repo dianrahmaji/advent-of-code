56eb487cf70c15e4dd63a76415cd78ff7992e4d7
Dian Rahmaji <dianrahmaji@gmail.com>
2023-02-02 08:45:21 +0700

    optimize 2020/01 part 1

--- 2020/01/index.ts
+++ 2020/01/index.ts
@@ -5,11 +5,16 @@ const content = getInput(__dirname)
   .map((v) => parseInt(v));
 
 function calculateEntries(input: number[]): number {
-  for (let i = 0; i < input.length - 1; i++) {
-    for (let j = i + 1; j < input.length; j++) {
-      if (input[i] + input[j] === 2020) return input[i] * input[j];
+  const map = new Map<number, number>();
+
+  for (let i = 0; i < input.length; i++) {
+    if (map.has(input[i])) {
+      return map.get(input[i])! * input[i];
     }
+
+    map.set(2020 - input[i], input[i]);
   }
+
   return 0;
 }
 
