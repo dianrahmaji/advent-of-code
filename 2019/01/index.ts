a5eead04ccf7e56d2e2afcc01a69a2c482576d21
Dian Rahmaji <dianrahmaji@gmail.com>
2023-02-04 06:34:32 +0700

    solve 2019/01 part 2

--- 2019/01/index.ts
+++ 2019/01/index.ts
@@ -8,8 +8,26 @@ function calculateFuel(mass: number): number {
   return Math.floor(mass / 3) - 2;
 }
 
+function calculateCumulativeFuel(mass: number): number {
+  let fuel = calculateFuel(mass);
+  let cumulativeFuel = fuel;
+
+  while (fuel > 0) {
+    fuel = Math.max(0, calculateFuel(fuel));
+    cumulativeFuel += fuel;
+  }
+
+  return cumulativeFuel;
+}
+
 /** Part 1 */
-const result = masses
+let result = masses
   .map((m) => calculateFuel(m))
   .reduce((acc, cur) => acc + cur, 0);
+console.log(result);
+
+/** Part 2 */
+result = masses
+  .map((m) => calculateCumulativeFuel(m))
+  .reduce((acc, prev) => acc + prev, 0);
 console.log(result);
