2cef6cf534497b848bfea9fb76ab83d7ef104f95
Dian Rahmaji <dianrahmaji@gmail.com>
2023-02-06 18:29:56 +0700

    solve 2017/01 part 2

--- 2017/01/index.ts
+++ 2017/01/index.ts
@@ -15,6 +15,20 @@ function sum(s: string): number {
   return matches.map((v) => parseInt(v)).reduce((acc, cur) => acc + cur, 0);
 }
 
+function sum2(s: string): number {
+  const matches = [];
+
+  for (let i = 0; i < s.length / 2; i++) {
+    if (s[i] === s[s.length / 2 + i]) matches.push(s[i]);
+  }
+
+  return matches.map((v) => parseInt(v)).reduce((acc, cur) => acc + cur, 0) * 2;
+}
+
 /** Part 1 */
 const result = s.map((v) => sum(v));
 console.log(result);
+
+/** Part 2 */
+const result2 = s.map((v) => sum2(v));
+console.log(result2);
