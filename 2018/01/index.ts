e038afb2013be6c8372c0fdfc0cc36a0aae54fca
Dian Rahmaji <dianrahmaji@gmail.com>
2023-02-06 08:03:34 +0700

    solve 2018/01 part 2

--- 2018/01/index.ts
+++ 2018/01/index.ts
@@ -5,3 +5,18 @@ const changes = parseLines(__dirname, parseInt);
 /** Part 1 */
 const result = changes.reduce((acc, cur) => acc + cur, 0);
 console.log(result);
+
+/** Part 2 */
+let set = new Set();
+let sum = 0;
+
+while (!set.has(sum)) {
+  for (let i = 0; i < changes.length; i++) {
+    if (set.has(sum)) {
+      console.log(sum);
+      break;
+    }
+    set.add(sum);
+    sum += changes[i];
+  }
+}
