99a1c428e558abad2196bbdfaa87f5622da4838e
Dian Rahmaji <dianrahmaji@gmail.com>
2023-02-09 07:43:12 +0700

    solve 2016/02 part 2

--- 2016/02/index.ts
+++ 2016/02/index.ts
@@ -8,6 +8,14 @@ const keypad = [
   [7, 8, 9],
 ];
 
+const bathroomKeypad = [
+  [null, null, "1", null, null],
+  [null, "2", "3", "4", null],
+  ["5", "6", "7", "8", "9"],
+  [null, "A", "B", "C", null],
+  [null, null, "D", null, null],
+];
+
 /** Part 1 */
 const result = directions.reduce(
   (acc, directions) => {
@@ -38,3 +46,38 @@ const result = directions.reduce(
 );
 
 console.log(result);
+
+/** Part 2 */
+const result2 = directions.reduce(
+  (acc, directions) => {
+    let { r, col, row } = acc;
+
+    for (const d of directions) {
+      switch (d) {
+        case "U":
+          if (bathroomKeypad[Math.max(0, row - 1)][col] !== null)
+            row = Math.max(0, row - 1);
+          break;
+        case "R":
+          if (bathroomKeypad[row][Math.min(4, col + 1)] !== null)
+            col = Math.min(4, col + 1);
+          break;
+        case "D":
+          if (bathroomKeypad[Math.min(4, row + 1)][col] !== null)
+            row = Math.min(4, row + 1);
+          break;
+        case "L":
+          if (bathroomKeypad[row][Math.max(0, col - 1)] !== null)
+            col = Math.max(0, col - 1);
+          break;
+      }
+    }
+
+    r = r + bathroomKeypad[row][col];
+
+    return { r, col, row };
+  },
+  { r: "", col: 0, row: 2 }
+);
+
+console.log(result2);
