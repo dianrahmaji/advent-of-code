25d0c7688fad8843e9a427b6c7a0be8862e5b945
Dian Rahmaji <dianrahmaji@gmail.com>
2023-02-16 11:12:36 +0700

    solve 2016/3 part 2

--- 2016/03/index.ts
+++ 2016/03/index.ts
@@ -24,3 +24,32 @@ let result = triangles.reduce((acc, triangle) => {
 }, 0);
 
 console.log(result);
+
+/** Part 2 */
+let result2 = 0;
+
+for (let row = 0; row < triangles.length; row += 3) {
+  for (let col = 0; col < triangles[row].length; col++) {
+    let comparable = [
+      triangles[row][col],
+      triangles[row + 1][col],
+      triangles[row + 2][col],
+    ];
+    // .sort((a, b) => a - b);
+
+    // if (comparable[0] + comparable[1] > comparable[2]) result2++;
+    let inValid = 0;
+    for (let i = 0; i < comparable.length; i++) {
+      let cur = i;
+      let next = (i + 1) % comparable.length;
+      let compare = (i + 2) % comparable.length;
+
+      if (comparable[cur] + comparable[next] <= comparable[compare]) {
+        inValid++;
+      }
+    }
+    if (inValid === 0) result2++;
+  }
+}
+
+console.log(result2);
