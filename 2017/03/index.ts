84576c624b07abf4eca03c12de636827069c24f8
Dian Rahmaji <dianrahmaji@gmail.com>
2023-02-19 10:01:51 +0700

    refactor solution 2017/03

--- 2017/03/index.ts
+++ 2017/03/index.ts
@@ -2,42 +2,13 @@ import { parseLines } from "../../utils";
 
 const data = parseLines(__dirname, parseInt);
 
-function getLayer(value: number): number {
-  return Math.ceil(Math.floor(Math.sqrt(value)) / 2);
-}
+function getDistance(value: number): number {
+  const size = Math.ceil(Math.sqrt(value));
+  const center = Math.ceil((size - 1) / 2);
 
-function getLayerBiggestNumber(layer: number): number {
-  return (layer * 2 + 1) ** 2;
-}
-
-function getCenter(root: number): number {
-  return Math.ceil(root / 2);
-}
-
-function getDistanceFromCenter(
-  value: number,
-  root: number,
-  center: number
-): number {
-  return (value % (root - 1)) - center;
-}
-
-// TODO: Handle case for squared number
-// TODO: Handle number is in the left side
-function getSteps(value: number): number {
-  const layer = getLayer(value);
-  const biggest = getLayerBiggestNumber(layer);
-  const rootOfBiggest = Math.sqrt(biggest);
-  const center = getCenter(rootOfBiggest);
-  const distanceFromCenter = getDistanceFromCenter(
-    value,
-    rootOfBiggest,
-    center
-  );
-
-  return layer + distanceFromCenter;
+  return Math.max(0, center - 1 + Math.abs(center - (value % size)));
 }
 
 /** Part 1 */
-const result = data.map((v) => getSteps(v));
+const result = data.map((v) => getDistance(v));
 console.log(result);
