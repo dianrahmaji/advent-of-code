bca6576d9b8d12f79776ddc87ee519707c3e5019
Dian Rahmaji <dianrahmaji@gmail.com>
2023-02-19 10:25:03 +0700

    solving 2017/03 part 2 (in progress)

--- 2017/03/index.ts
+++ 2017/03/index.ts
@@ -9,6 +9,27 @@ function getDistance(value: number): number {
   return Math.max(0, center - 1 + Math.abs(center - (value % size)));
 }
 
+function getValue() {}
+
+function getNextValueInSpiral(value: number): number {
+  const coordinateMap = new Map<[number, number], number>();
+  const nextDirectionMap = new Map<string, string>([
+    ["R", "U]"],
+    ["U", "L"],
+    ["L", "D"],
+    ["D", "R"],
+  ]);
+
+  let x = 0;
+  let y = 0;
+
+  return value;
+}
+
 /** Part 1 */
-const result = data.map((v) => getDistance(v));
+let result = data.map((v) => getDistance(v));
+console.log(result);
+
+/** Part 2 */
+result = data.map((v) => getNextValueInSpiral(v));
 console.log(result);
