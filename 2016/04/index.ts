b4b25b8a1f0fa2675c04ee8daa9e3d8d76cf8d39
Dian Rahmaji <dianrahmaji@gmail.com>
2023-02-21 10:17:13 +0700

    solve 2016/04 part 1

---
+++ 2016/04/index.ts
@@ -0,0 +1,47 @@
+import { getLines } from "../../utils";
+
+const rooms = getLines(__dirname);
+
+function isValidRoom(room: {
+  name: string;
+  sector: number;
+  checksum: string;
+}): boolean {
+  const { name, sector, checksum } = room;
+  const charMap = new Map<string, number>();
+
+  for (let char of name) {
+    charMap.set(char, (charMap.get(char) || 0) + 1);
+  }
+
+  const charSorted = [...charMap].sort(([, i], [, j]) => j - i);
+
+  const lengthMap = new Map<number, string[]>();
+
+  for (let [char, length] of charSorted) {
+    lengthMap.set(length, [...(lengthMap.get(length) || []), char].sort());
+  }
+
+  let cs = "";
+
+  for (let [, char] of lengthMap) {
+    for (let c of char) {
+      cs += c;
+    }
+  }
+
+  return checksum === cs.slice(0, 5);
+}
+
+/** Part 1 */
+let result = rooms
+  .map((r) => ({
+    //TODO: Parse with RegEx
+    name: r.slice(0, -11).split("-").join(""),
+    sector: parseInt(r.slice(-11).slice(1, 4)),
+    checksum: r.slice(-11).slice(5, 10),
+  }))
+  .filter(isValidRoom)
+  .reduce((acc, cur) => acc + cur.sector, 0);
+
+console.log(result);
