e06b85d8f2a4977990d146b992fa2ce922244a98
Dian Rahmaji <dianrahmaji@gmail.com>
2023-02-24 08:45:46 +0700

    solve 2020/03 part 2

--- 2020/03/index.ts
+++ 2020/03/index.ts
@@ -2,12 +2,39 @@ import { getInput } from "../../utils";
 
 const area = getInput(__dirname).split("\n");
 
-/** Part 1 */
-let trees = 0;
+function countTrees(area: string[], right: number, down: number): number {
+  let trees = 0;
 
-for (let row = 1; row < area.length; row++) {
-  let col = (3 + (row - 1) * 3) % area[row].length;
-  if (area[row][col] === "#") trees++;
+  for (let row = down, i = 1; row < area.length; row += down) {
+    let col =
+      down > right
+        ? (row / down) % area[row].length
+        : (right + (row - 1) * right) % area[row].length;
+
+    if (area[row][col] === "#") {
+      trees++;
+    }
+  }
+
+  return trees;
 }
 
-console.log(trees);
+/** Part 1 */
+let result = countTrees(area, 3, 1);
+
+console.log(result);
+
+/** Part 2 */
+const slopes = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+];
+
+result = slopes
+  .map(([right, down]) => countTrees(area, right, down))
+  .reduce((acc, cur) => acc * cur, 1);
+
+console.log(result);
