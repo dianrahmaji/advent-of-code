7f5be4fd2769894c8e89859c4fd778cbf6676bb0
Dian Rahmaji <dianrahmaji.jakmall@gmail.com>
2023-09-30 14:20:40 +0700

    feat(2015-01): solve part 2 with typescript

--- src/2015/01/index.test.ts
+++ src/2015/01/index.test.ts
@@ -2,17 +2,26 @@ import { join } from "path";
 import { readFileSync } from "fs";
 import { describe, expect, it } from "bun:test";
 
-import { partOne } from ".";
+import { partOne, partTwo } from ".";
+
+const [testCasePartOne, testCasePartTwo] = JSON.parse(
+	readFileSync(join(import.meta.dir, "sample.json"), "utf-8"),
+) as [string, number][][];
 
 describe("2015 day 01", () => {
-	describe("sample", () => {
-		const sample = JSON.parse(
-			readFileSync(join(import.meta.dir, "sample.json"), "utf-8"),
-		) as [string, number][];
-		it.each(sample)(
+	describe("Part One", () => {
+		it.each(testCasePartOne)(
 			"has direction %s and goes to floor %i",
 			(direction, expected) => {
 				expect(partOne(direction)).toBe(expected);
+			},
+		);
+	});
+	describe("Part Two", () => {
+		it.each(testCasePartTwo)(
+			"has direction %s and enter the basement at the instruction number %i",
+			(direction, expected) => {
+				expect(partTwo(direction)).toBe(expected);
 			},
 		);
 	});
--- src/2015/01/index.ts
+++ src/2015/01/index.ts
@@ -3,6 +3,8 @@ enum DirectionOption {
 	Down = ")",
 }
 
+const BASEMENT = -1;
+
 export function partOne(input: string) {
 	return input.split("").reduce((currentFloor, instruction) => {
 		switch (instruction) {
@@ -14,4 +16,24 @@ export function partOne(input: string) {
 				return currentFloor;
 		}
 	}, 0);
+}
+
+export function partTwo(input: string) {
+	let currentFloor = 0;
+	let instuctionPosition = 0;
+
+	while (currentFloor !== BASEMENT) {
+		switch (input[instuctionPosition]) {
+			case DirectionOption.Up:
+				currentFloor++;
+				break;
+			case DirectionOption.Down:
+				currentFloor--;
+				break;
+		}
+
+		instuctionPosition++;
+	}
+
+	return instuctionPosition;
 }
