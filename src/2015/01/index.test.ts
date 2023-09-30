6cd117240402b0a8a5a7c9115a280883c3f6c1bb
Dian Rahmaji <dianrahmaji.jakmall@gmail.com>
2023-09-30 11:16:37 +0700

    chore(typescript): implement eslint and prettier

--- src/2015/01/index.test.ts
+++ src/2015/01/index.test.ts
@@ -1,14 +1,19 @@
 import { join } from "path";
 import { readFileSync } from "fs";
-import { describe, expect, it } from 'bun:test';
+import { describe, expect, it } from "bun:test";
 
-import { partOne } from '.'
+import { partOne } from ".";
 
-describe('2015 day 01', () => {
-  describe('sample', () => {
-    const sample = JSON.parse(readFileSync(join(import.meta.dir, 'sample.json'), 'utf-8')) as [string,number][]
-    it.each(sample)('has direction %s and goes to floor %i', (direction, expected) => {
-      expect(partOne(direction)).toBe(expected);
-    })
-  })
-})
+describe("2015 day 01", () => {
+	describe("sample", () => {
+		const sample = JSON.parse(
+			readFileSync(join(import.meta.dir, "sample.json"), "utf-8"),
+		) as [string, number][];
+		it.each(sample)(
+			"has direction %s and goes to floor %i",
+			(direction, expected) => {
+				expect(partOne(direction)).toBe(expected);
+			},
+		);
+	});
+});
